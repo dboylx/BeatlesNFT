8417c19729649d1cd3d8b956096da116dadf9f48
jory lee <dboylx@gmail.com>
2022-09-25 06:04:15 +0800

    change the public sales price

--- Beatles.sol
+++ Beatles.sol
@@ -31,7 +31,7 @@ contract Beatles is
     uint256 public constant PRE_SALES_PRICE = 0.01 ether;
 
     // nft public sales price
-    uint256 public constant PUBLIC_SALES_PRICE = 0.05 ether;
+    uint256 public constant PUBLIC_SALES_PRICE = 0.005 ether;
 
 
     // ------------------------------------------- variable
